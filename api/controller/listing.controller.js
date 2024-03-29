d09765266f39ed5b1469d36e1d9299b2088a68c6
zhh312 <fjzhenghonghao@gmail.com>
2024-03-28 22:47:00 -0400

    create update listing api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -12,16 +12,40 @@ export const createListing = async (req, res, next) =>{
 }
 
 export const deleteListing = async (req, res, next) =>{
-    const listing = await Listing.findById(req.params.id)
-    if(!listing){
-        return next(errorHandler(404, "Listing not found")  )
-    }
-    if(listing.userRef !== req.user.id){
-        return next(errorHandler(401, "You can only delete your own listings"))
-    }
+    
     try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+          return next(errorHandler(404, "Listing not found"));
+        }
+        if (listing.userRef !== req.user.id) {
+          return next(
+            errorHandler(401, "You can only delete your own listings")
+          );
+        }
         await Listing.findByIdAndDelete(req.params.id)
         return res.status(200).json("Listing has been deleted")
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updateListing = async (req, res, next) =>{
+    
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+          return next(errorHandler(404, "Listing not found"));
+        }
+        if (listing.userRef !== req.user.id) {
+          return next(
+            errorHandler(401, "You can only update your own listings")
+          );
+        }
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true})
+        return res.status(200).json(updatedListing)
     } catch (error) {
         next(error)
     }
