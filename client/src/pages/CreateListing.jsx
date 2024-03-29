ea71a26795c0bd704161864a609e889d83381a3b
zhh312 <fjzhenghonghao@gmail.com>
2024-03-29 03:50:33 -0400

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -264,7 +264,8 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular Price</p>
-                <span className='text-xs'>($ / Month)</span>
+                {formData.type === "rent" && (<span className='text-xs'>($ / Month)</span>)}
+                
               </div>
             </div>
             {formData.offer && (
@@ -281,7 +282,7 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted Price</p>
-                  <span className='text-xs'>($ / Month)</span>
+                  {formData.type === "rent" && (<span className='text-xs'>($ / Month)</span>)}
                 </div>
               </div>
             )}
