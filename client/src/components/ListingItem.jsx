573381303621e562128c32828b3e312151e53932
zhh312 <fjzhenghonghao@gmail.com>
2024-03-29 23:43:54 -0400

    create the listing item component and show listings

---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+export default function ListingItem({ listing }) {
+  return (
+    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
+      <Link to={`/listing/${listing._id}`}>
+        <img
+          src={listing.imageUrls[0]}
+          alt='cover image'
+          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
+        />
+        <div className='p-3 flex flex-col gap-2 w-full'>
+          <p className='text-lg font-semibold text-slate-700 truncate'>
+            {listing.name}
+          </p>
+          <div className='flex items-center gap-1'>
+            <FaMapMarkerAlt className='h-4 w-4 text-green-700' />
+            <p className='text-sm text-gray-600 truncate w-full'>
+              {listing.address}
+            </p>
+          </div>
+          <p className='line-clamp-2 text-sm text-gray-600 h-[2.5rem]'>
+            {listing.description}
+          </p>
+          <p className='text-slate-500 mt-2 font-semibold'>
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && " / month"}
+          </p>
+          <div className='text-slate-700 gap-4 flex'>
+            <div className='font-bold text-xs'>
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds`
+                : `${listing.bedrooms} bed`}
+            </div>
+            <div className='font-bold text-xs'>
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} baths`
+                : `${listing.bathrooms} bath`}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
