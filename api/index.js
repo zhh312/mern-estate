bdea5f154b90054993bc3da60e2096497e59d850
zhh312 <fjzhenghonghao@gmail.com>
2024-03-30 02:59:37 -0400

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.router.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
   .catch((error) => {
     console.log("Error connecting to MongoDB", error);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -28,6 +31,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
