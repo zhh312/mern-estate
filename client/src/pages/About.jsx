ad09261650d640bf7bacd8683e73d1a05c4c1e22
zhh312 <fjzhenghonghao@gmail.com>
2024-03-30 02:47:45 -0400

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Honghao Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Honghao Estate is a premier real estate agency specializing in
+        facilitating property transactions within prime neighborhoods. Our
+        seasoned team is devoted to ensuring a seamless buying and selling
+        process, offering unparalleled service.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our core mission is to guide clients in achieving their real estate
+        objectives with tailored advice and a profound understanding of local
+        markets. Whether buying, selling, or renting, we're committed to
+        supporting you every step of the journey.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Backed by extensive industry experience, our agents deliver top-tier
+        service, aiming to transform property transactions into exciting and
+        rewarding experiences for our valued clients.
+      </p>
+    </div>
+  );
 }
